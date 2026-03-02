2039117492dd3a4b5e3f80ab7226c33ab19ef40d
Ryuya Uetake <38969179+deruloxxx@users.noreply.github.com>
2026-03-02 16:50:11 +1300

    test: verify husky pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,32 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    name: "prettier",
+    ...prettierConfig,
+  },
+  {
+    name: "custom-rules",
+    rules: {
+      "prefer-const": "error",
+      "no-var": "error",
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
